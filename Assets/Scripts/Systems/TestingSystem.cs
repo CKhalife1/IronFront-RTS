d9e53834b4b5986cac09f73c18b8e2891b4a2aba
Charles Khalife <charles.khalife23@gmail.com>
2026-04-15 14:57:59 +0300

    Implement ShootAttack feature and update related prefabs
    - Added ShootAttackAuthoring script to define shooting behavior with a timer.
    - Created ShootAttack system to manage shooting logic based on target entities.
    - Updated BaseSolider, Conscript, and GI prefabs to utilize the new ShootAttack component.
    - Adjusted timerMax values for consistency across prefabs.
    - Enhanced UnitSelectionManager to ensure proper selection of entities with the new component structure.

--- Assets/Scripts/Systems/TestingSystem.cs
+++ Assets/Scripts/Systems/TestingSystem.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 partial struct TestingSystem : ISystem
 {
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
-    {
-        int unitCount = 0;
+    // [BurstCompile]
+    // public void OnUpdate(ref SystemState state)
+    // {
+    //     int unitCount = 0;
 
-        foreach (
-            RefRW<Friendly> friendly
-            in SystemAPI.Query<
-            RefRW<Friendly>>())  {
-                unitCount++;
-            }
+    //     foreach (
+    //         RefRW<Friendly> friendly
+    //         in SystemAPI.Query<
+    //         RefRW<Friendly>>())  {
+    //             unitCount++;
+    //         }
 
-            Debug.Log("Friendly count: " + unitCount);
-    }
+    //         Debug.Log("Friendly count: " + unitCount);
+    // }
 }
